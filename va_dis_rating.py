6b637ffbe75db0f60735a5e4fbc3145712332f7c
scottschmidl <sjschmidl@gmail.com>
2022-09-06 14:31:15 -0500

    added some robustness

--- va_dis_rating.py
+++ va_dis_rating.py
@@ -7,7 +7,15 @@ Date: 09/06/2022
 class DisabilityRatingCalculator:
 
     def calculate(self, ratings):
-        print(ratings)
+        while True:
+            correct = input("Please verify ratings are correct, enter: y or n: ").lower()
+
+            if correct not in ["y", "yes"]:
+                print("Oops, try again")
+                ratings = input("Please enter your ratings separated by spaces: ")
+                ratings = [int(rating) for rating in ratings.split()]
+
+            else: break
 
         if max(ratings) == 100:
             return 100
@@ -18,7 +26,7 @@ class DisabilityRatingCalculator:
 
             while (total_body > 0) or (percentage <= 100):
                 if not ratings:
-                    return percentage
+                    return round(percentage, -1)
 
                 else:
                     max_rating = ratings.pop(ratings.index(max(ratings)))
